455cb557b303693a599e134c88017c6526a65b61
Victualler <victuallerdev@gmail.com>
2026-04-01 19:16:57 -0600

    a

--- src/muqsit/invmenu/session/PlayerWindowDispatcher.php
+++ src/muqsit/invmenu/session/PlayerWindowDispatcher.php
@@ -117,7 +117,7 @@ final class PlayerWindowDispatcher{
 
 		assert($this->window_id !== null);
 		$session = $this->session->player->getNetworkSession();
-		$session->sendDataPacket(ContainerClosePacket::create($this->window_id, WindowTypes::CONTAINER, true));
+		$session->sendDataPacket(ContainerClosePacket::create($this->window_id, WindowTypes::CONTAINER,  false));
 		$this->n_finalization_acks++;
 		foreach($this->packets as $packet){
 			$session->sendDataPacket($packet);
